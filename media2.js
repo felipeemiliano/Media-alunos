94d8ce4bfcf9ecbeb038e6a820ea2e404c53d1f6
felipeemiliano <111158344+felipeemiliano@users.noreply.github.com>
2022-08-29 21:31:02 -0300

    Create media2.js

---
+++ media2.js
@@ -0,0 +1,7 @@
+var nome = "Felipe "
+console.log (nome)
+var total = 21
+var nota1 = 5
+var nota2 = 8
+var faltam = (total - nota1, nota2)
+console.log ('Você Precisa tirar' , (faltam) , 'para ser aprovado.')
